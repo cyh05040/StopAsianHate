bfd8b321d0722be0050175ee513ccebfe142833d
Yen-Hao Chen <yen-haochen@Yen-Haos-MacBook-Pro.local>
2021-04-12 00:00:52 -0400

    Assignment 4: Backend update

--- Backend/my-app-serverless/handler.js
+++ Backend/my-app-serverless/handler.js
@@ -25,16 +25,38 @@ module.exports.hello = async (event) => {
 
 module.exports.help = async (event) => {
   // You'll only receive events for GET /help requests
+  const firebaseTokenVerifier = require('firebase-token-verifier')
+  const projectId = 'firebase-project-id'
   if (event.path === '/help' && event.httpMethod === 'GET') {
-    return {
-      statusCode: 200,
-      body: JSON.stringify([{
-        organization: 'EnoughIsEnough',
-        images: 'enoughIsenough.jpg',
-        type: 'donation',
-        description: '#EnoughIsEnough is a grassroots initiative by Asian American business owners in New York City who came together to take action on the recent surge in nationwide hate crimes against the Asian American community. Frustrated by the lack of media coverage on the recent violence, we are rallying all the industry homies to encourage people both in and out of the Asian community to realize the power of their voice.',			
-        url: 'https://givebutter.com/enoughisenough',
-      }])
+
+    // check the header named Authorization
+    const token = event.headers['Authorization']
+    // If no token is provided, or it is "", return a 401
+    if (!token) {
+      return {
+        statusCode: 401
+      }
+    }
+    try {
+      // validate the token from the request
+      const decoded = await firebaseTokenVerifier.validate(token, projectId)
+       // user is now confirmed to be authorized, return the data
+      return {
+        statusCode: 200,
+        body: JSON.stringify([{
+          organization: 'EnoughIsEnough',
+          images: 'enoughIsenough.jpg',
+          type: 'donation',
+          description: '#EnoughIsEnough is a grassroots initiative by Asian American business owners in New York City who came together to take action on the recent surge in nationwide hate crimes against the Asian American community. Frustrated by the lack of media coverage on the recent violence, we are rallying all the industry homies to encourage people both in and out of the Asian community to realize the power of their voice.',			
+          url: 'https://givebutter.com/enoughisenough',
+        }])
+      } 
+    } catch (err) {
+      // the token was invalid,
+      console.error(err)
+      return {
+        statusCode: 401
+      }
     }
   }
 };
